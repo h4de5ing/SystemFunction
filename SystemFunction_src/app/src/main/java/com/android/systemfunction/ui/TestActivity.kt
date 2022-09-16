f0e8c4c86797c0c865061a94454a70570f856488
h4de5ing <lex109@qq.com>
2022-09-16 15:07:22 +0800

    update

---
+++ SystemFunction_src/app/src/main/java/com/android/systemfunction/ui/TestActivity.kt
@@ -0,0 +1,11 @@
+package com.android.systemfunction.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
