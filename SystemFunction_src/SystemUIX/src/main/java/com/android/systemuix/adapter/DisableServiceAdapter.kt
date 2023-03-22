5064040528ab66fd477d7c3aa4b654bf87dd1c8f
h4de5ing <lex109@qq.com>
2023-03-22 17:36:59 +0800

    update

--- SystemFunction_src/SystemUIX/src/main/java/com/android/systemuix/adapter/DisableServiceAdapter.kt
+++ SystemFunction_src/SystemUIX/src/main/java/com/android/systemuix/adapter/DisableServiceAdapter.kt
@@ -2,13 +2,18 @@ package com.android.systemuix.adapter
 
 import android.app.AlertDialog
 import android.content.pm.PackageInfo
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.PixelFormat
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
-import com.android.systemlib.byteArray2Drawable
-import com.android.systemlib.drawable2ByteArray
 import com.android.systemuix.R
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.github.h4de5ing.base.exec
+import java.io.ByteArrayOutputStream
 
 class DisableServiceAdapter(layoutRes: Int = R.layout.item_app_list) :
     BaseQuickAdapter<PackageInfo, BaseViewHolder>(layoutRes) {
@@ -34,9 +39,8 @@ class DisableServiceAdapter(layoutRes: Int = R.layout.item_app_list) :
         }
     }
 
-    private fun getAppIsEnabled(packageName: String): Boolean {
-        return context.packageManager.getPackageInfo(packageName, 0).applicationInfo.enabled
-    }
+    private fun getAppIsEnabled(packageName: String): Boolean =
+        context.packageManager.getPackageInfo(packageName, 0).applicationInfo.enabled
 
     private fun confirmDialog(title: String, message: String, block: () -> Unit) {
         val builder: AlertDialog.Builder = AlertDialog.Builder(context)
@@ -45,5 +49,42 @@ class DisableServiceAdapter(layoutRes: Int = R.layout.item_app_list) :
         builder.setNegativeButton(android.R.string.cancel, null)
         builder.setPositiveButton(android.R.string.ok) { _, _ -> block() }
         builder.show()
+    }
+
+    //序列化 Drawable->Bitmap->ByteArray
+    private fun drawable2ByteArray(icon: Drawable): ByteArray =
+        bitmap2ByteArray(drawable2Bitmap(icon))
+
+    private fun bitmap2ByteArray(bitmap: Bitmap): ByteArray {
+        val bao = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, bao)
+        return bao.toByteArray()
+    }
+
+    //反序列化 ByteArray->Bitmap->Drawable
+    private fun byteArray2Drawable(byteArray: ByteArray): Drawable? {
+        val bitmap = byteArray2Bitmap(byteArray)
+        return if (bitmap == null) null else BitmapDrawable(bitmap)
+    }
+
+    private fun byteArray2Bitmap(byteArray: ByteArray): Bitmap? {
+        return if (byteArray.isNotEmpty()) BitmapFactory.decodeByteArray(
+            byteArray,
+            0,
+            byteArray.size
+        ) else null
+    }
+
+    private fun drawable2Bitmap(icon: Drawable): Bitmap {
+        val bitmap =
+            Bitmap.createBitmap(
+                icon.intrinsicWidth,
+                icon.intrinsicHeight,
+                if (icon.opacity == PixelFormat.OPAQUE) Bitmap.Config.RGB_565 else Bitmap.Config.ARGB_8888
+            )
+        val canvas = Canvas(bitmap)
+        icon.setBounds(0, 0, icon.intrinsicWidth, icon.intrinsicHeight)
+        icon.draw(canvas)
+        return bitmap
     }
 }
