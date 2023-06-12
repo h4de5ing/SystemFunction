38f984828e33faaa4008018d6f6e6e18a53fbdd2
dearhj <dear_hj@163.com>
2023-06-12 15:07:14 +0800

    1、新增Android13的相关库

---
+++ SystemFunction_src/Android13/src/main/java/com/android/android13/Android13.kt
@@ -0,0 +1,111 @@
+package com.android.android13
+
+import android.hardware.ICameraService
+import android.hardware.ICameraServiceListener
+import android.hardware.ISensorPrivacyListener
+import android.hardware.ISensorPrivacyManager
+import android.hardware.SensorPrivacyManager
+import android.os.Build
+import android.os.RemoteException
+import android.os.ServiceManager
+import android.service.SensorPrivacyIndividualEnabledSensorProto
+import android.service.SensorPrivacyToggleSourceProto
+import androidx.annotation.RequiresApi
+
+
+@RequiresApi(Build.VERSION_CODES.S)
+fun disableSensor13(isDisable: Boolean, sensor: Int) {
+    val sensors = when (sensor) {
+        1 -> SensorPrivacyManager.Sensors.MICROPHONE
+        2 -> SensorPrivacyManager.Sensors.CAMERA
+        else -> SensorPrivacyIndividualEnabledSensorProto.UNKNOWN
+    }
+    val spm =
+        ISensorPrivacyManager.Stub.asInterface(ServiceManager.getService("sensor_privacy")) as ISensorPrivacyManager
+    spm.setToggleSensorPrivacy(0, SensorPrivacyToggleSourceProto.SETTINGS, sensors, !isDisable)
+}
+
+
+private var isp: ISensorPrivacyManager? = null
+private var iSensorPrivacyListener: ISensorPrivacyListener1? = null
+fun addIndividualSensorPrivacyListener(change: (Boolean) -> Unit) {
+    try {
+        if (Build.VERSION.SDK_INT == 33) {
+            iSensorPrivacyListener = ISensorPrivacyListener1(change)
+            isp =
+                ISensorPrivacyManager.Stub.asInterface(ServiceManager.getService("sensor_privacy"))
+            isp?.addToggleSensorPrivacyListener(iSensorPrivacyListener)
+            isp?.setToggleSensorPrivacy(
+                0,
+                SensorPrivacyToggleSourceProto.SETTINGS,
+                SensorPrivacyManager.Sensors.CAMERA,
+                false
+            )
+        }
+    } catch (e: RemoteException) {
+        e.printStackTrace()
+    }
+}
+
+class ISensorPrivacyListener1(val change: (Boolean) -> Unit) : ISensorPrivacyListener.Stub() {
+    override fun onSensorPrivacyChanged(p0: Int, p1: Int, enabled: Boolean) {
+        if (Build.VERSION.SDK_INT == 33) {
+            isp?.setToggleSensorPrivacy(
+                0,
+                SensorPrivacyToggleSourceProto.SETTINGS,
+                SensorPrivacyManager.Sensors.CAMERA,
+                false
+            )
+            change(enabled)
+        }
+    }
+}
+
+
+fun removeIndividualSensorPrivacyListener() {
+    try {
+        if (Build.VERSION.SDK_INT == 33) isp?.removeToggleSensorPrivacyListener(
+            iSensorPrivacyListener
+        )
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+fun cameraListener(StatusChanged: ((Int) -> Unit), TorchStatusChanged: ((Int) -> Unit)) {
+    try {
+        val cameraService =
+            ICameraService.Stub.asInterface(ServiceManager.getService("media.camera"))
+        cameraService.addListener(object : ICameraServiceListener.Stub() {
+            override fun onStatusChanged(status: Int, cameraId: String) {
+                //status -2 占用 1释放
+                println("onStatusChanged " + (if (status == -2) "占用" else "释放") + " cameraId:" + cameraId)
+                StatusChanged(status)
+            }
+
+            override fun onPhysicalCameraStatusChanged(
+                status: Int,
+                cameraId: String,
+                physicalCameraId: String
+            ) {
+                println("onPhysicalCameraStatusChanged $status cameraId:$cameraId")
+            }
+
+            override fun onTorchStatusChanged(status: Int, cameraId: String) {
+                println("onTorchStatusChanged status=\" + status + \" cameraId=\" + cameraId + \",\" + (status == 0 ? cameraId + \" 相机被占用\" : cameraId + \" 相机释放")
+                if (cameraId != "2") {
+                    TorchStatusChanged(status)
+                }
+            }
+
+            override fun onTorchStrengthLevelChanged(s: String, i: Int) {}
+            override fun onCameraAccessPrioritiesChanged() {}
+            override fun onCameraOpened(cameraId: String, clientPackageId: String) {}
+            override fun onCameraClosed(cameraId: String) {
+                println("onCameraClosed  $cameraId")
+            }
+        })
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
