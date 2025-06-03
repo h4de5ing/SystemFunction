d1c48c83be22a009bf281be01cb6964ac30a5027
h4de5ing <lex109@qq.com>
2025-06-03 11:37:35 +0800

    update

--- SystemFunction_src/droidwall/src/main/java/com/android/droidwall/receiver/BootReceiver.kt
+++ SystemFunction_src/droidwall/src/main/java/com/android/droidwall/receiver/BootReceiver.kt
@@ -3,12 +3,12 @@ package com.android.droidwall.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.android.droidwall.services.ForegroundService
+import com.android.droidwall.services.DForegroundService
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         try {
-            context.startService(Intent(context, ForegroundService::class.java))
+            context.startService(Intent(context, DForegroundService::class.java))
         } catch (e: Exception) {
             e.printStackTrace()
         }
