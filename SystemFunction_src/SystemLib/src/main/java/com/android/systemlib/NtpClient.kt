39e549b9052d92023025fed0df288884fca6377c
h4de5ing <lex109@qq.com>
2025-11-06 15:08:09 +0800

    update

--- SystemFunction_src/SystemLib/src/main/java/com/android/systemlib/NtpClient.kt
+++ SystemFunction_src/SystemLib/src/main/java/com/android/systemlib/NtpClient.kt
@@ -1,27 +1,17 @@
-package com.android.systemlib;
+package com.android.systemlib
 
-import android.os.SystemClock;
+import android.os.SystemClock
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-
-public class NtpClient {
-    public static final int NTP_TIME_OUT_MILLISECOND = 3000;
-    private static final int REFERENCE_TIME_OFFSET = 16;
-    private static final int ORIGINATE_TIME_OFFSET = 24;
-    private static final int RECEIVE_TIME_OFFSET = 32;
-    private static final int TRANSMIT_TIME_OFFSET = 40;
-    private static final int NTP_PACKET_SIZE = 48;
-
-    private static final int NTP_PORT = 123;
-    private static final int NTP_MODE_CLIENT = 3;
-    private static final int NTP_VERSION = 3;
-    // Number of seconds between Jan 1, 1900 and Jan 1, 1970
-    // 70 years plus 17 leap days
-    private static final long OFFSET_1900_TO_1970 = ((365L * 70L) + 17L) * 24L * 60L * 60L;
-
-
+/**
+ * 常用校时服务器
+ * edu.ntp.org.cn
+ * ntp.aliyun.com
+ * ntp.ntsc.ac.cn 中国科学院国家授时中心
+ */
+class NtpClient {
     /**
      * Sends an SNTP request to the given host and processes the response.
      *
@@ -29,88 +19,108 @@ public class NtpClient {
      * @param timeout network timeout in milliseconds.
      * @return time if the transaction was successful.
      */
-    public long requestTime(String host, int timeout) {
-        long currentTime = System.currentTimeMillis();
-        try (DatagramSocket socket = new DatagramSocket()) {
-            socket.setSoTimeout(timeout);
-            InetAddress address = InetAddress.getByName(host);
-            byte[] buffer = new byte[NTP_PACKET_SIZE];
-            DatagramPacket request = new DatagramPacket(buffer, buffer.length, address, NTP_PORT);
-            buffer[0] = NTP_MODE_CLIENT | (NTP_VERSION << 3);
-            // get current time and write it to the request packet
-            long requestTime = System.currentTimeMillis();
-            long requestTicks = SystemClock.elapsedRealtime();
-            writeTimeStamp(buffer, TRANSMIT_TIME_OFFSET, requestTime);
-            socket.send(request);
-            // read the response
-            DatagramPacket response = new DatagramPacket(buffer, buffer.length);
-            socket.receive(response);
-            long responseTicks = SystemClock.elapsedRealtime();
-            long responseTime = requestTime + (responseTicks - requestTicks);
-            // extract the results
-            long originateTime = readTimeStamp(buffer, ORIGINATE_TIME_OFFSET);
-            long receiveTime = readTimeStamp(buffer, RECEIVE_TIME_OFFSET);
-            long transmitTime = readTimeStamp(buffer, TRANSMIT_TIME_OFFSET);
-            long roundTripTime = responseTicks - requestTicks - (transmitTime - receiveTime);
-            long clockOffset = ((receiveTime - originateTime) + (transmitTime - responseTime)) / 2;
-            currentTime = responseTime + clockOffset;
-        } catch (Exception e) {
-            System.out.println(host + " 校时错误 " + e.getMessage());
-            e.printStackTrace();
-            return currentTime;
+    fun requestTime(host: String, timeout: Int): Long {
+        var currentTime = System.currentTimeMillis()
+        try {
+            DatagramSocket().use { socket ->
+                socket.soTimeout = timeout
+                val address = InetAddress.getByName(host)
+                val buffer = ByteArray(NTP_PACKET_SIZE)
+                val request = DatagramPacket(buffer, buffer.size, address, NTP_PORT)
+                buffer[0] = (NTP_MODE_CLIENT or (NTP_VERSION shl 3)).toByte()
+                // get current time and write it to the request packet
+                val requestTime = System.currentTimeMillis()
+                val requestTicks = SystemClock.elapsedRealtime()
+                writeTimeStamp(buffer, requestTime)
+                socket.send(request)
+                // read the response
+                val response = DatagramPacket(buffer, buffer.size)
+                socket.receive(response)
+                val responseTicks = SystemClock.elapsedRealtime()
+                val responseTime = requestTime + (responseTicks - requestTicks)
+                // extract the results
+                val originateTime = readTimeStamp(buffer, ORIGINATE_TIME_OFFSET)
+                val receiveTime = readTimeStamp(buffer, RECEIVE_TIME_OFFSET)
+                val transmitTime = readTimeStamp(buffer, TRANSMIT_TIME_OFFSET)
+//                val roundTripTime = responseTicks - requestTicks - (transmitTime - receiveTime)
+                val clockOffset =
+                    ((receiveTime - originateTime) + (transmitTime - responseTime)) / 2
+                currentTime = responseTime + clockOffset
+            }
+        } catch (e: Exception) {
+            println(host + " 校时错误 " + e.message)
+            e.printStackTrace()
+            return currentTime
         }
-        return currentTime;
+        return currentTime
     }
 
     /**
      * Reads an unsigned 32 bit big endian number from the given offset in the buffer.
      */
-    private long read32(byte[] buffer, int offset) {
-        byte b0 = buffer[offset];
-        byte b1 = buffer[offset + 1];
-        byte b2 = buffer[offset + 2];
-        byte b3 = buffer[offset + 3];
+    private fun read32(buffer: ByteArray, offset: Int): Long {
+        val b0 = buffer[offset]
+        val b1 = buffer[offset + 1]
+        val b2 = buffer[offset + 2]
+        val b3 = buffer[offset + 3]
 
         // convert signed bytes to unsigned values
-        int i0 = ((b0 & 0x80) == 0x80 ? (b0 & 0x7F) + 0x80 : b0);
-        int i1 = ((b1 & 0x80) == 0x80 ? (b1 & 0x7F) + 0x80 : b1);
-        int i2 = ((b2 & 0x80) == 0x80 ? (b2 & 0x7F) + 0x80 : b2);
-        int i3 = ((b3 & 0x80) == 0x80 ? (b3 & 0x7F) + 0x80 : b3);
+        val i0 = (if ((b0.toInt() and 0x80) == 0x80) (b0.toInt() and 0x7F) + 0x80 else b0.toInt())
+        val i1 = (if ((b1.toInt() and 0x80) == 0x80) (b1.toInt() and 0x7F) + 0x80 else b1.toInt())
+        val i2 = (if ((b2.toInt() and 0x80) == 0x80) (b2.toInt() and 0x7F) + 0x80 else b2.toInt())
+        val i3 = (if ((b3.toInt() and 0x80) == 0x80) (b3.toInt() and 0x7F) + 0x80 else b3.toInt())
 
-        return ((long) i0 << 24) + ((long) i1 << 16) + ((long) i2 << 8) + (long) i3;
+        return (i0.toLong() shl 24) + (i1.toLong() shl 16) + (i2.toLong() shl 8) + i3.toLong()
     }
 
     /**
      * Reads the NTP time stamp at the given offset in the buffer and returns
      * it as a system time (milliseconds since January 1, 1970).
      */
-    private long readTimeStamp(byte[] buffer, int offset) {
-        long seconds = read32(buffer, offset);
-        long fraction = read32(buffer, offset + 4);
-        return ((seconds - OFFSET_1900_TO_1970) * 1000) + ((fraction * 1000L) / 0x100000000L);
+    private fun readTimeStamp(buffer: ByteArray, offset: Int): Long {
+        val seconds = read32(buffer, offset)
+        val fraction = read32(buffer, offset + 4)
+        return ((seconds - OFFSET_1900_TO_1970) * 1000) + ((fraction * 1000L) / 0x100000000L)
     }
 
     /**
      * Writes system time (milliseconds since January 1, 1970) as an NTP time stamp
      * at the given offset in the buffer.
      */
-    private void writeTimeStamp(byte[] buffer, int offset, long time) {
-        long seconds = time / 1000L;
-        long milliseconds = time - seconds * 1000L;
-        seconds += OFFSET_1900_TO_1970;
+    private fun writeTimeStamp(buffer: ByteArray, time: Long) {
+        var offset = TRANSMIT_TIME_OFFSET
+        var seconds = time / 1000L
+        val milliseconds = time - seconds * 1000L
+        seconds += OFFSET_1900_TO_1970
 
         // write seconds in big endian format
-        buffer[offset++] = (byte) (seconds >> 24);
-        buffer[offset++] = (byte) (seconds >> 16);
-        buffer[offset++] = (byte) (seconds >> 8);
-        buffer[offset++] = (byte) (seconds >> 0);
+        buffer[offset++] = (seconds shr 24).toByte()
+        buffer[offset++] = (seconds shr 16).toByte()
+        buffer[offset++] = (seconds shr 8).toByte()
+        buffer[offset++] = (seconds shr 0).toByte()
 
-        long fraction = milliseconds * 0x100000000L / 1000L;
+        val fraction = milliseconds * 0x100000000L / 1000L
         // write fraction in big endian format
-        buffer[offset++] = (byte) (fraction >> 24);
-        buffer[offset++] = (byte) (fraction >> 16);
-        buffer[offset++] = (byte) (fraction >> 8);
+        buffer[offset++] = (fraction shr 24).toByte()
+        buffer[offset++] = (fraction shr 16).toByte()
+        buffer[offset++] = (fraction shr 8).toByte()
         // low order bits should be random data
-        buffer[offset++] = (byte) (Math.random() * 255.0);
+        buffer[offset++] = (Math.random() * 255.0).toInt().toByte()
+    }
+
+    companion object {
+        const val NTP_TIME_OUT_MILLISECOND: Int = 3000
+        private const val ORIGINATE_TIME_OFFSET = 24
+        private const val RECEIVE_TIME_OFFSET = 32
+        private const val TRANSMIT_TIME_OFFSET = 40
+        private const val NTP_PACKET_SIZE = 48
+
+        private const val NTP_PORT = 123
+        private const val NTP_MODE_CLIENT = 3
+        private const val NTP_VERSION = 3
+
+        // Number of seconds between Jan 1, 1900 and Jan 1, 1970
+        // 70 years plus 17 leap days
+        private const val OFFSET_1900_TO_1970 = ((365L * 70L) + 17L) * 24L * 60L * 60L
     }
 }
